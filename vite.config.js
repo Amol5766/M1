9c6dcf2abef25065d996010a28e9fa03b9f07349
Amol M <117634099+Amol5766@users.noreply.github.com>
2024-10-14 11:21:24 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,7 @@ import React from 'react';
 export default {
   build: {
     rollupOptions: {
-      external: [
-        React,
-        '@fortawesome/react-fontawesome',
-        'react-intersection-observer' // Add this line
-      ]
+      external: [ React ]
     }
   }
 };
