13be7bc7181a21d35db1f5365a65435d2954e5fb
Amol M <117634099+Amol5766@users.noreply.github.com>
2024-10-25 10:27:04 +0530

    Update Model.jsx

--- src/components/Model/Model.jsx
+++ src/components/Model/Model.jsx
@@ -36,36 +36,33 @@ const AnimatedText = () => {
     }
   });
 
-  return (
+   return (
     <Text
       ref={textRef}
-      position={[0, 1, 0]} // Position the text above the model
-      fontSize={0.8}
-      color="red"
-      anchorX="center"
-      anchorY="middle"
+      position={[0, 2, 0]} // Position above the model
+      fontSize={1} // Adjust size as needed
+      color="#FF5733" // Set the color of the text
     >
-AMOL❤️‍🔥
+      Bite Me!!
     </Text>
   );
 };
 
 const ModelWrapper = () => {
-    return (
-      <Canvas camera={{ position: [0, -20, 85], fov: 20 }}> {/* Adjusted camera position and FOV */}
-        <ambientLight intensity={3.8} />
-        <directionalLight intensity={10} position={[0, 0, 5]} />
-        <pointLight intensity={1} position={[-10, -10, -10]} />
-  
-        {/* Render your loaded model */}
-        <Suspense fallback={<Html><div>Loading model...</div></Html>}>
-          <Model />
-          <AnimatedText /> {/* 3D animated text */}
-        </Suspense>
-  
-        <OrbitControls enablePan={true} enableZoom={true} />
-      </Canvas>
-    );
-  };
-  
+  return (
+    <Canvas>
+      <ambientLight intensity={2.5} />
+      <pointLight position={[10, 10, 10]} />
+
+      {/* Render your loaded model */}
+      <Suspense fallback={<Html><div>Loading model...</div></Html>}>
+        <Model />
+        <AnimatedText /> {/* 3D animated text */}
+      </Suspense>
+
+      <OrbitControls />
+    </Canvas>
+  );
+};
+
 export default ModelWrapper;
